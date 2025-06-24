2bb55ff0be999360cfcdf5d14e707f4bd53cced0
Pritesh Bhuravane <144042331+PriteshBhuravane@users.noreply.github.com>
2025-06-24 20:11:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "0.0.0.0",                    // Exposes the app to outside world (Render)
+    port: parseInt(process.env.PORT) || 5173, // Uses Render's PORT or defaults locally
   },
 })
